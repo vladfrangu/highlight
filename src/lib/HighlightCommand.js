0fa98197fca2e7f7b8817cc8d2ddb0498c131c18
Frangu Vlad <kingdgrizzle@gmail.com>
2018-08-16 17:22:13 +0300

    QoL

--- src/lib/HighlightCommand.js
+++ src/lib/HighlightCommand.js
@@ -4,19 +4,22 @@ const { Command } = require("klasa");
 class HighlightCommand extends Command {
 	async redirectDisplay (message, display, time = 120000) {
 		const { guild: { settings: { bot } } } = message;
-		if (display.pages.length > 1 && bot.redirect && bot.channel && message.channel.id !== bot.channel) {
-			await message.channel.send(
+		if (bot.redirect && bot.channel && message.channel.id !== bot.channel) {
+			message.channel.send(
 				new MessageEmbed()
 					.setColor(0x3669FA)
 					.setDescription(`This command would be too spammy for this channel!\nPlease go to <#${bot.channel}> to see the results.`)
-			);
+					.setFooter("This message self-destructs in 10 seconds")
+			).then(m => m.delete(10000));
 			const channel = message.guild.channels.get(bot.channel);
 			if (channel) {
 				await channel.send(`${message.author},`);
-				return display.run(
-					await channel.send("Loading..."),
-					{ time, stop: display.pages.length > 1, filter: (reaction, user) => user === message.author, jump: false, firstLast: false }
-				);
+				return display.pages.length === 1 ?
+					channel.send(display.pages[0]) :
+					display.run(
+						await channel.send("Loading..."),
+						{ time, stop: display.pages.length > 1, filter: (reaction, user) => user === message.author, jump: false, firstLast: false }
+					);
 			}
 		}
 		return display.pages.length === 1 ?
