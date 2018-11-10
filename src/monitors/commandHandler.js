cfe3b297be8572cc7c5d9c5a96bbf767b6cd67e1
Frangu Vlad <kingdgrizzle@gmail.com>
2018-11-10 12:39:09 +0100

    VPS Commit

--- src/monitors/commandHandler.js
+++ src/monitors/commandHandler.js
@@ -1,20 +1,20 @@
-const { Monitor, Stopwatch, util: { regExpEsc } } = require("klasa");
+const { Monitor, Stopwatch, util: { regExpEsc } } = require('klasa');
 
 module.exports = class extends Monitor {
-	constructor (...args) {
+
+	constructor(...args) {
 		super(...args, { ignoreOthers: false });
+		this.ignoreEdits = !this.client.options.commandEditing;
 		this.prefixes = new Map();
 		this.prefixMention = null;
-		this.prefixMentionLength = null;
-		this.nick = new RegExp("^<@!");
-		this.prefixFlags = this.client.options.prefixCaseInsensitive ? "i" : "";
+		this.mentionOnly = null;
+		this.prefixFlags = this.client.options.prefixCaseInsensitive ? 'i' : '';
 	}
 
-	async run (message) {
-		if (this.client.user.bot && message.guild && !message.guild.me) await message.guild.members.fetch(this.client.user);
-		if (message.guild && !message.channel.postable) return;
-		if (message.content === this.client.user.toString() || (message.guild && message.content === message.guild.me.toString())) {
-			message.send({
+	async run(message) {
+		if (message.guild && !message.guild.me) await message.guild.members.fetch(this.client.user);
+		if (!message.channel.postable) return undefined;
+		if (this.mentionOnly.test(message.content)) return message.send({
 				embed: {
 					color: 0x3669FA,
 					title: `Hi!`,
@@ -24,97 +24,80 @@ module.exports = class extends Monitor {
 					},
 				},
 			});
-			return;
-		}
 
-		const { command, prefix, prefixLength } = this.parseCommand(message);
-		if (!command) return;
+		const { commandText, prefix, prefixLength } = this.parseCommand(message);
+		if (!commandText) return undefined;
 
-		const validCommand = this.client.commands.get(command);
-		if (!validCommand) {
-			if (this.client.listenerCount("commandUnknown")) this.client.emit("commandUnknown", message, command);
-			return;
-		}
+		const command = this.client.commands.get(commandText);
+		if (!command) return this.client.emit('commandUnknown', message, commandText);
 
-		if (!message.member && validCommand.needsMember) await message.guild.members.fetch(message.author);
-		if (message.member) await message.member.settings.sync(true);
-
-		const timer = new Stopwatch();
-		if (this.client.options.typing) message.channel.startTyping();
-		message._registerCommand({ command: validCommand, prefix, prefixLength });
-
-		try {
-			await this.client.inhibitors.run(message, validCommand);
-		} catch (response) {
-			if (this.client.options.typing) message.channel.stopTyping();
-			this.client.emit("commandInhibited", message, validCommand, response);
-			return;
-		}
-
-		this.runCommand(message, timer);
+		return this.runCommand(message._registerCommand({ command, prefix, prefixLength }));
 	}
 
-	parseCommand (message) {
-		const { regex: prefix, length: prefixLength } = this.getPrefix(message);
-		if (!prefix) return { command: false };
-		return {
-			command: message.content.slice(prefixLength).trim().split(" ")[0].toLowerCase(),
-			prefix,
-			prefixLength,
-		};
+	parseCommand(message) {
+		const result = this.customPrefix(message) || this.mentionPrefix(message) || this.naturalPrefix(message) || this.prefixLess(message);
+		return result ? {
+			commandText: message.content.slice(result.length).trim().split(' ')[0].toLowerCase(),
+			prefix: result.regex,
+			prefixLength: result.length
+		} : { commandText: false };
 	}
 
-	getPrefix (message) {
-		if (this.prefixMention.test(message.content)) return { length: this.nick.test(message.content) ? this.prefixMentionLength + 1 : this.prefixMentionLength, regex: this.prefixMention };
-		if (message.guildSettings.disableNaturalPrefix !== true && this.client.options.regexPrefix) {
-			const results = this.client.options.regexPrefix.exec(message.content);
-			if (results) return { length: results[0].length, regex: this.client.options.regexPrefix };
+	customPrefix({ content, guildSettings: { prefix } }) {
+		if (!prefix) return null;
+		for (const prf of Array.isArray(prefix) ? prefix : [prefix]) {
+			const testingPrefix = this.prefixes.get(prf) || this.generateNewPrefix(prf);
+			if (testingPrefix.regex.test(content)) return testingPrefix;
 		}
-		const prefix = message.guildSettings.prefix || this.client.options.prefix;
-		if (Array.isArray(prefix)) {
-			for (let i = prefix.length - 1; i >= 0; i--) {
-				const testingPrefix = this.prefixes.get(prefix[i]) || this.generateNewPrefix(prefix[i]);
-				if (testingPrefix.regex.test(message.content)) return testingPrefix;
-			}
-		} else if (prefix) {
-			const testingPrefix = this.prefixes.get(prefix) || this.generateNewPrefix(prefix);
-			if (testingPrefix.regex.test(message.content)) return testingPrefix;
-		}
-		return false;
+		return null;
 	}
 
-	generateNewPrefix (prefix) {
+	mentionPrefix({ content }) {
+		const prefixMention = this.prefixMention.exec(content);
+		return prefixMention ? { length: prefixMention[0].length, regex: this.prefixMention } : null;
+	}
+
+	naturalPrefix({ content, guildSettings: { disableNaturalPrefix } }) {
+		if (disableNaturalPrefix || !this.client.options.regexPrefix) return null;
+		const results = this.client.options.regexPrefix.exec(content);
+		return results ? { length: results[0].length, regex: this.client.options.regexPrefix } : null;
+	}
+
+	prefixLess({ channel: { type } }) {
+		return this.client.options.noPrefixDM && type === 'dm' ? { length: 0, regex: null } : null;
+	}
+
+	generateNewPrefix(prefix) {
 		const prefixObject = { length: prefix.length, regex: new RegExp(`^${regExpEsc(prefix)}`, this.prefixFlags) };
 		this.prefixes.set(prefix, prefixObject);
 		return prefixObject;
 	}
 
-	async runCommand (message, timer) {
+	async runCommand(message) {
+		const timer = new Stopwatch();
+		if (this.client.options.typing) message.channel.startTyping();
 		try {
-			await message.prompter.run();
-		} catch (error) {
-			if (this.client.options.typing) message.channel.stopTyping();
-			return this.client.emit("commandError", message, message.command, message.params, error);
+			await this.client.inhibitors.run(message, message.command);
+			try {
+				await message.prompter.run();
+				const subcommand = message.command.subcommands ? message.params.shift() : undefined;
+				const commandRun = subcommand ? message.command[subcommand](message, message.params) : message.command.run(message, message.params);
+				timer.stop();
+				const response = await commandRun;
+				this.client.finalizers.run(message, message.command, response, timer);
+				this.client.emit('commandSuccess', message, message.command, message.params, response);
+			} catch (error) {
+				this.client.emit('commandError', message, message.command, message.params, error);
+			}
+		} catch (response) {
+			this.client.emit('commandInhibited', message, message.command, response);
 		}
-
-		const subcommand = message.command.subcommands ? message.params.shift() : undefined;
-		const commandRun = subcommand ? message.command[subcommand](message, message.params) : message.command.run(message, message.params);
-
 		if (this.client.options.typing) message.channel.stopTyping();
-		timer.stop();
-
-		try {
-			const response = await commandRun;
-			await this.client.finalizers.run(message, response, timer);
-			return this.client.emit("commandSuccess", message, message.command, message.params, response);
-		} catch (error) {
-			return this.client.emit("commandError", message, message.command, message.params, error);
-		}
 	}
 
-	async init () {
-		this.ignoreEdits = !this.client.options.commandEditing;
+	init() {
 		this.prefixMention = new RegExp(`^<@!?${this.client.user.id}>`);
-		this.prefixMentionLength = this.client.user.id.length + 3;
+		this.mentionOnly = new RegExp(`^<@!?${this.client.user.id}>$`);
 	}
+
 };
