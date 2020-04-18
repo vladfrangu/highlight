2761c44a002db677e8bb5631663f1e888cec4bea
Vlad Frangu <kingdgrizzle@gmail.com>
2020-04-18 22:03:50 +0300

    src: Fix extendable apparently

--- src/extendables/Message#prompt.ts
+++ src/extendables/Message#prompt.ts
@@ -1,14 +1,15 @@
-import { Extendable, ExtendableOptions } from 'klasa';
-import { ApplyOptions } from '@skyra/decorators';
+import { Extendable, ExtendableStore } from 'klasa';
 import { Message } from 'discord.js';
 
-@ApplyOptions<ExtendableOptions>({
-	appliesTo: [Message],
-})
 export default class extends Extendable {
+
+	constructor(store: ExtendableStore, file: string[], directory: string) {
+		super(store, file, directory, { appliesTo: [Message] });
+	}
+
 	async prompt(this: Message, content: string) {
 		const message = await this.channel.send(content);
-		const responses = await this.channel.awaitMessages(msg => msg.author === this.author, { time: 30000, max: 1 });
+		const responses = await this.channel.awaitMessages((msg) => msg.author === this.author, { time: 30000, max: 1 });
 		message.delete().catch((error) => this.client.emit('wtf', error));
 		if (responses.size === 0) throw this.language.get('MESSAGE_PROMPT_TIMEOUT');
 		return responses.first();
