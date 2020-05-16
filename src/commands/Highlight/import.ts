49e1533575fa4185a78799eea24f031af0951096
bdistin <bdistin@gmail.com>
2020-05-16 17:07:10 -0500

    feature(import everything)

--- src/commands/Highlight/import.ts
+++ src/commands/Highlight/import.ts
@@ -1,6 +1,6 @@
 import { Command, CommandOptions, KlasaMessage, KlasaGuild } from 'klasa';
 import { ApplyOptions } from '@skyra/decorators';
-import { MessageEmbed, Util } from 'discord.js';
+import { MessageEmbed, Util, GuildMember } from 'discord.js';
 import { pluralize } from '../../lib/utils/Util';
 
 @ApplyOptions<CommandOptions>({
@@ -8,12 +8,14 @@ import { pluralize } from '../../lib/utils/Util';
 	description: 'Import your words or regular expressions from a different guild.',
 	permissionLevel: 2,
 	runIn: ['text'],
-	usage: '<regex|words:default> <guild:guild>',
+	usage: '<regex|words|everything:default> <guild:guild>',
 	usageDelim: ' ',
 	extendedHelp: [
-		"→ If you want to import words from a guild",
-		'`{prefix}import [words] 479665229396049920` → Specifying `words` is optional as it is the default subcommand',
+		"→ If you want to import all words and regular expressions from a guild",
+		'`{prefix}import [everything] 479665229396049920` → Specifying `everything` is optional as it is the default subcommand',
 		"",
+		"→ If you want to import words from a guild",
+		"`{prefix}import words 479665229396049920` → Adds your words from that guild, if they aren't added already.",
 		"→ Importing your regular expressions from a guild",
 		"`{prefix}import regex 479665229396049920` → Adds your regular expressions from that guild, if they aren't added already.",
 	].join('\n'),
@@ -22,11 +24,10 @@ import { pluralize } from '../../lib/utils/Util';
 export default class extends Command {
 	needsMember = true;
 
-	async words(message: KlasaMessage, [guild]: [KlasaGuild]) {
+	async everything(message: KlasaMessage, [guild]: [KlasaGuild]) {
 		if (!message.guild || !message.member) throw new Error('Unreachable');
 
 		const member = await guild.members.fetch(message.author);
-		await member.settings.sync(true);
 
 		if (!member) {
 			return message.send(new MessageEmbed()
@@ -35,17 +36,41 @@ export default class extends Command {
 			);
 		}
 
-		const words = member.settings.get('words') as string[];
+		await member.settings.sync(true);
 
-		const previousWords = message.member.settings.get('words') as string[];
+		const addedWords = await this._words(message, member);
+		const addedExpressions = await this._regex(message, member);
 
-		const addedWords = [];
+		const added = [...addedWords, ...addedExpressions];
 
-		for (const word of words)
-			if (!previousWords.includes(word)) addedWords.push(word);
+		const embed = new MessageEmbed()
+			.setColor(0x43B581)
+			.setDescription('No new words or regular expressions have been added..');
 
-		await message.member.settings.update('words', addedWords, { arrayAction: 'add' });
-		message.guild.addWords(addedWords, message.author.id);
+		if (added.length) {
+			embed.setTitle(`The following words or expressions have been added to your list`)
+				// eslint-disable-next-line @typescript-eslint/require-array-sort-compare
+				.setDescription(`- \`${added.sort().map((word) => Util.escapeMarkdown(word)).join('`\n- `')}\``);
+		}
+
+		return message.send(embed);
+	}
+
+	async words(message: KlasaMessage, [guild]: [KlasaGuild]) {
+		if (!message.guild || !message.member) throw new Error('Unreachable');
+
+		const member = await guild.members.fetch(message.author);
+
+		if (!member) {
+			return message.send(new MessageEmbed()
+				.setColor(0x43B581)
+				.setDescription('You do not appear to be in that guild.'),
+			);
+		}
+
+		await member.settings.sync(true);
+
+		const addedWords = await this._words(message, member);
 
 		const embed = new MessageEmbed()
 			.setColor(0x43B581)
@@ -64,7 +89,6 @@ export default class extends Command {
 		if (!message.guild || !message.member) throw new Error('Unreachable');
 
 		const member = await guild.members.fetch(message.author);
-		await member.settings.sync(true);
 
 		if (!member) {
 			return message.send(new MessageEmbed()
@@ -73,27 +97,19 @@ export default class extends Command {
 			);
 		}
 
-		const regularExpressions = member.settings.get('regularExpressions') as string[];
+		await member.settings.sync(true);
 
-		const previousExpressions = message.member.settings.get('regularExpressions') as string[];
-
-		const added = [];
-
-		for (const regex of regularExpressions)
-			if (!previousExpressions.includes(regex)) added.push(regex);
-
-		await message.member.settings.update('regularExpressions', added, { arrayAction: 'add' });
-		message.guild.addRegularExpressions(added, message.author.id);
+		const addedExpressions = await this._regex(message, member);
 
 		const embed = new MessageEmbed()
 			.setColor(0x43B581)
 			.setDescription('No new regular expressions were added..');
 
-		if (added.length) {
+		if (addedExpressions.length) {
 			embed
-				.setTitle(`The following ${pluralize(added.length, 'expression has', 'expressions have')} been added to your list`)
+				.setTitle(`The following ${pluralize(addedExpressions.length, 'expression has', 'expressions have')} been added to your list`)
 				// eslint-disable-next-line @typescript-eslint/require-array-sort-compare
-				.setDescription(`- \`${added.sort().map((expression) => Util.escapeMarkdown(expression, {
+				.setDescription(`- \`${addedExpressions.sort().map((expression) => Util.escapeMarkdown(expression, {
 					bold: false,
 					italic: false,
 					spoiler: false,
@@ -104,5 +120,41 @@ export default class extends Command {
 		}
 
 		return message.send(embed);
+	}
+
+	async _words(message: KlasaMessage, member: GuildMember) {
+		if (!message.guild || !message.member) throw new Error('Unreachable');
+
+		const words = member.settings.get('words') as string[];
+
+		const previousWords = message.member.settings.get('words') as string[];
+
+		const addedWords = [];
+
+		for (const word of words)
+			if (!previousWords.includes(word)) addedWords.push(word);
+
+		await message.member.settings.update('words', addedWords, { arrayAction: 'add' });
+		message.guild.addWords(addedWords, message.author.id);
+
+		return addedWords;
+	}
+
+	async _regex(message: KlasaMessage, member: GuildMember) {
+		if (!message.guild || !message.member) throw new Error('Unreachable');
+
+		const regularExpressions = member.settings.get('regularExpressions') as string[];
+
+		const previousExpressions = message.member.settings.get('regularExpressions') as string[];
+
+		const addedExpressions = [];
+
+		for (const regex of regularExpressions)
+			if (!previousExpressions.includes(regex)) addedExpressions.push(regex);
+
+		await message.member.settings.update('regularExpressions', addedExpressions, { arrayAction: 'add' });
+		message.guild.addRegularExpressions(addedExpressions, message.author.id);
+
+		return addedExpressions;
 	}
 }
